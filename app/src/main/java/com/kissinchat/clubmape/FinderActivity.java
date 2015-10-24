504d4ab5196ac30c0e8336fe7eac65082e01a5e7
Viljami Anttonen <viljami.anttonen@gmail.com>
2015-10-25 02:55:48 +0300

    Started working on compass

--- app/src/main/java/com/kissinchat/clubmape/FinderActivity.java
+++ app/src/main/java/com/kissinchat/clubmape/FinderActivity.java
@@ -1,14 +1,29 @@
 package com.kissinchat.clubmape;
 
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.os.Handler;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.RotateAnimation;
+import android.widget.TextView;
 
-public class FinderActivity extends AppCompatActivity {
+public class FinderActivity extends AppCompatActivity implements SensorEventListener {
     private View bottle;
+    private TextView text;
+    private SensorManager mSensorManager;
+    private Sensor mAccelerometer;
+    private Sensor mMagnetometer;
+    private float[] mLastAccelerometer = new float[3];
+    private float[] mLastMagnetometer = new float[3];
+    private boolean mLastAccelerometerSet = false;
+    private boolean mLastMagnetometerSet = false;
+    private float[] mR = new float[9];
+    private float[] mOrientation = new float[3];
+    private float mCurrentDegree = 0f;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,32 +31,69 @@ public class FinderActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_finder);
 
+        // Visuals
         bottle = findViewById(R.id.bottleView);
+        text = (TextView) findViewById(R.id.distanceText);
+
+        // Compass stuff
+        mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        mAccelerometer = (Sensor) mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        mMagnetometer = (Sensor) mSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
     }
 
     @Override
     protected void onPostCreate(Bundle savedInstanceState) {
         super.onPostCreate(savedInstanceState);
+    }
 
-        final Handler h = new Handler();
-        final int delay = 2000; //milliseconds
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_GAME);
+        mSensorManager.registerListener(this, mMagnetometer, SensorManager.SENSOR_DELAY_GAME);
+    }
 
-        h.postDelayed(new Runnable() {
-            public void run() {
-                spinBottle(bottle.getRotation() + 180);
-                h.postDelayed(this, delay);
-            }
-        }, delay);
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mSensorManager.unregisterListener(this, mAccelerometer);
+        mSensorManager.unregisterListener(this, mMagnetometer);
     }
 
     private void spinBottle(float to) {
         float from = bottle.getRotation();
 
-        RotateAnimation rotate = new RotateAnimation(from, to, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
-        rotate.setDuration(2000);
-        rotate.setRepeatCount(0);
-        rotate.setFillAfter(true);
+//        RotateAnimation rotate = new RotateAnimation(from, to, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+//        rotate.setDuration(200);
+//        rotate.setRepeatCount(0);
+//        rotate.setFillAfter(true);
 
-        bottle.startAnimation(rotate);
+//        bottle.startAnimation(rotate);
+
+        bottle.setRotation(to);
+        text.setText(String.valueOf(to));
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if (event.sensor == mAccelerometer) {
+            System.arraycopy(event.values, 0, mLastAccelerometer, 0, event.values.length);
+            mLastAccelerometerSet = true;
+        } else if (event.sensor == mMagnetometer) {
+            System.arraycopy(event.values, 0, mLastMagnetometer, 0, event.values.length);
+            mLastMagnetometerSet = true;
+        }
+        if (mLastAccelerometerSet && mLastMagnetometerSet) {
+            SensorManager.getRotationMatrix(mR, null, mLastAccelerometer, mLastMagnetometer);
+            SensorManager.getOrientation(mR, mOrientation);
+            float azimuthInRadians = mOrientation[0];
+            float azimuthInDegress = (float)(Math.toDegrees(azimuthInRadians)+360)%360;
+            spinBottle(azimuthInDegress);
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        // not in use
     }
 }
